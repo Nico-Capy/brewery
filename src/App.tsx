50b1eac6dbbe818027564b97558f0db7d85bfb5a
Nico-Capy <nicolacorradini97@yahoo.com>
2023-04-13 13:23:40 +0200

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,12 +8,14 @@ function App() {
   return (
     <div className="App">
       <div>
-        
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl text-red-600 font-bold underline">Vite + React</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/elements/navbar.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import { Link, Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
+
