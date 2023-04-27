1aabf41de13ab1c2920e40fdab499d07bc2f1d9a
Nico-Capy <nicolacorradini97@yahoo.com>
2023-04-27 15:17:18 +0200

    Alignment

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -25,29 +25,29 @@ function Discover() {
             </Center>
             
             <Text p="1rem" fontFamily="Capy" fontSize="2xl" color="white">
-  <p>Style: blonde ale</p>
-  <br />
-  <p>Alcohol: 4,3%</p>
-  <br />
-  <p>Rating:</p>
-  <Flex alignItems="center">
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box>
-      <FontAwesomeIcon icon={faStar} color="white" />
-    </Box>
-  </Flex>
-</Text>
+              <p>Style: blonde ale</p>
+              <br />
+              <p>Alcohol: 4,3%</p>
+              <br />
+              <p>Rating:</p>
+              <Flex alignItems="center">
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box>
+                  <FontAwesomeIcon icon={faStar} color="white" />
+                </Box>
+              </Flex>
+            </Text>
           </Box>
           <Box m='6' mt='0' boxShadow="2xl" backgroundColor='blue.500' borderRadius="1rem" h={{ base: '90vh', sm: '72vh', md: '72vh', lg: '80vh', xl: '80vh' }} w={{ base: '90vw', sm: '90vw', md: '90vw', lg: '26vw', xl: '26vw' }}>
             <Center>
@@ -59,29 +59,29 @@ function Discover() {
               </Text>
             </Center>
             <Text p="1rem" fontFamily="Capy" fontSize="2xl" color="white">
-  <p>Style: stout</p>
-  <br />
-  <p>Alcohol: 7,9%</p>
-  <br />
-  <p>Rating:</p>
-  <Flex alignItems="center">
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box>
-      <FontAwesomeIcon icon={faStar} color="white" />
-    </Box>
-  </Flex>
-</Text>
+              <p>Style: stout</p>
+              <br />
+              <p>Alcohol: 7,9%</p>
+              <br />
+              <p>Rating:</p>
+              <Flex alignItems="center">
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box>
+                  <FontAwesomeIcon icon={faStar} color="white" />
+                </Box>
+              </Flex>
+            </Text>
           </Box>
           <Box m='6' mt='0' boxShadow="2xl" backgroundColor='blue.500' borderRadius="1rem" h={{ base: '90vh', sm: '72vh', md: '72vh', lg: '80vh', xl: '80vh' }} w={{ base: '90vw', sm: '90vw', md: '90vw', lg: '26vw', xl: '26vw' }}>
             <Center>
@@ -93,29 +93,29 @@ function Discover() {
               </Text>
             </Center>
             <Text p="1rem" fontFamily="Capy" fontSize="2xl" color="white">
-  <p>Style: experimental</p>
-  <br />
-  <p>Alcohol: 12%</p>
-  <br />
-  <p>Rating:</p>
-  <Flex alignItems="center">
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box>
-      <FontAwesomeIcon icon={faStar} color="white" />
-    </Box>
-  </Flex>
-</Text>
+              <p>Style: experimental</p>
+              <br />
+              <p>Alcohol: 12%</p>
+              <br />
+              <p>Rating:</p>
+              <Flex alignItems="center">
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box>
+                  <FontAwesomeIcon icon={faStar} color="white" />
+                </Box>
+              </Flex>
+            </Text>
           </Box>
         </Flex>
       </Center>
@@ -131,29 +131,29 @@ function Discover() {
               </Text>
             </Center>
             <Text p="1rem" fontFamily="Capy" fontSize="2xl" color="white">
-  <p>Style: brown ale</p>
-  <br />
-  <p>Alcohol: 6,96%</p>
-  <br />
-  <p>Rating:</p>
-  <Flex alignItems="center">
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box>
-      <FontAwesomeIcon icon={faStar} color="white" />
-    </Box>
-  </Flex>
-</Text>
+              <p>Style: brown ale</p>
+              <br />
+              <p>Alcohol: 6,96%</p>
+              <br />
+              <p>Rating:</p>
+              <Flex alignItems="center">
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box>
+                  <FontAwesomeIcon icon={faStar} color="white" />
+                </Box>
+              </Flex>
+            </Text>
           </Box>
           <Box m='6' mt='0' boxShadow="2xl" backgroundColor='blue.500' borderRadius="1rem" h={{ base: '90vh', sm: '72vh', md: '72vh', lg: '80vh', xl: '80vh' }} w={{ base: '90vw', sm: '90vw', md: '90vw', lg: '26vw', xl: '26vw' }}>
             <Center>
@@ -165,29 +165,29 @@ function Discover() {
               </Text>
             </Center>
             <Text p="1rem" fontFamily="Capy" fontSize="2xl" color="white">
-  <p>Style: blonde ale</p>
-  <br />
-  <p>Alcohol: 11,8%</p>
-  <br />
-  <p>Rating:</p>
-  <Flex alignItems="center">
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box mr="2">
-      <FontAwesomeIcon icon={faStar} color="yellow" />
-    </Box>
-    <Box>
-      <FontAwesomeIcon icon={faStar} color="white" />
-    </Box>
-  </Flex>
-</Text>
+              <p>Style: blonde ale</p>
+              <br />
+              <p>Alcohol: 11,8%</p>
+              <br />
+              <p>Rating:</p>
+              <Flex alignItems="center">
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box mr="2">
+                  <FontAwesomeIcon icon={faStar} color="yellow" />
+                </Box>
+                <Box>
+                  <FontAwesomeIcon icon={faStar} color="white" />
+                </Box>
+              </Flex>
+            </Text>
           </Box>
         </Flex>
       </Center>
