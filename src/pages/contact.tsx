9ee7fc9a5e305c6a66bf4013f69afcc1bde1d456
Nico-Capy <nicolacorradini97@yahoo.com>
2023-05-08 23:31:38 +0200

    Finished contact page for the day

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -59,15 +59,15 @@ const Contact = () => {
         <VStack spacing={4} fontFamily="American Typewriter">
           <FormControl id="name" isRequired>
             <FormLabel>Name</FormLabel>
-            <Input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+            <Input type="text" name="name" value={formData.name} onChange={handleInputChange} placeholder="Enter your name" required pattern=".{1,}" title="Please enter your name" />
           </FormControl>
           <FormControl id="email" isRequired>
             <FormLabel>Email</FormLabel>
-            <Input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+            <Input type="email" name="email" value={formData.email} onChange={handleInputChange} placeholder="Enter your email" required pattern=".{1,}" title="Please enter your email" />
           </FormControl>
           <FormControl id="message" isRequired>
             <FormLabel>Message</FormLabel>
-            <Textarea name="message" value={formData.message} onChange={handleInputChange} />
+            <Textarea name="message" value={formData.message} onChange={handleInputChange} placeholder="You get extra credit if the message is a compliment" required pattern=".{1,}" title="Please enter a message" />
           </FormControl>
           <Button type="submit" colorScheme="blue">
             Send
