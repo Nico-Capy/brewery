1a5d06c14f2ba4ea7986b1bb9cbcd75d9a0ea3d7
Nico-Capy <nicolacorradini97@yahoo.com>
2023-05-12 15:26:20 +0200

    Cutting off drinks function

--- src/pages/beerclicker.tsx
+++ src/pages/beerclicker.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Button, Center, Flex, Image, Text } from '@chakra-ui/react';
+import beerImage from '/beerclicker.svg';
 
 function Clicker() {
   const [beerCount, setBeerCount] = useState(0);
@@ -7,17 +8,56 @@ function Clicker() {
   const [isRoundOnTheHouseVisible, setRoundOnTheHouseVisible] = useState(false);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const randomTime = Math.floor(Math.random() * (16 - 8 + 1) + 8); // generate random time between 8 and 16 seconds
-      setTimeout(() => {
-        setRoundOnTheHouseVisible(true); // show the "Round on the house" button
+    let intervalId: number | undefined;
+    let timeoutId: number | undefined;
+    const showButton = () => {
+      const randomTime = Math.floor(Math.random() * (16 - 8 + 1) + 8);
+      timeoutId = setTimeout(() => {
+        setRoundOnTheHouseVisible(true);
+        timeoutId = setTimeout(() => {
+          setRoundOnTheHouseVisible(false);
+        }, 3000);
       }, 1000 * randomTime);
-    }, 10000); // check every 10 seconds
-
+    };
+    intervalId = setInterval(showButton, 10000);
     return () => {
-      clearInterval(intervalId); // clean up the interval when component unmounts
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
     };
   }, []);
+  
+  useEffect(() => {
+    let intervalId: number | undefined;
+    let timeoutId: number | undefined;
+    const showButton = () => {
+      const randomTime = Math.floor(Math.random() * (61 - 32 + 1) + 32); // generate random number between 32 and 61
+      timeoutId = setTimeout(() => {
+        const randomPoints = Math.floor(Math.random() * (42 - 4 + 1) + 4);
+        setBeerCount((prevBeerCount) => prevBeerCount - randomPoints);
+        const notification = document.createElement("div");
+        notification.innerText = `You drank too much, I'm cutting you off! -${randomPoints} beers`;
+        notification.style.position = "fixed";
+        notification.style.top = "50%";
+        notification.style.left = "50%";
+        notification.style.transform = "translate(-50%, -50%)";
+        notification.style.backgroundColor = "crimson";
+        notification.style.color = "white";
+        notification.style.fontFamily = "American Typewriter";
+        notification.style.fontSize = "1rem";
+        notification.style.padding = "0.5rem";
+        notification.style.borderRadius = "0rem";
+        document.body.appendChild(notification);
+        timeoutId = setTimeout(() => {
+          document.body.removeChild(notification);
+        }, 2300);
+      }, randomTime * 1000); // convert to milliseconds
+    };
+    intervalId = setInterval(showButton, 2000);
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
+    };
+  }, []);  
 
   const handleBeerClick = (points: number) => {
     setBeerCount((prevBeerCount) => prevBeerCount + points);
@@ -45,52 +85,27 @@ function Clicker() {
       <Text fontFamily="American Typewriter" fontSize="2xl" mb="4">
         Click the beer to drink it
       </Text>
-      <Box
-        w="40vh"
-        h="60vh"
-        bg="transparent"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Button
-          onClick={() => handleBeerClick(1)}
-          backgroundColor='transparent'
-          _hover={{ backgroundColor: "transparent" }}
-          _active={{ backgroundColor: "transparent" }}
-          _focus={{ outline: "none" }}
-          border="none"
-          m={6}
-          p={0}
-        >
+      <Box w="40vh" h="60vh" bg="transparent" display="flex" justifyContent="center" alignItems="center">
+        <Button onClick={() => handleBeerClick(1)} backgroundColor='transparent' _hover={{ backgroundColor: "transparent" }} _active={{ backgroundColor: "transparent" }} _focus={{ outline: "none" }} border="none" m={6} p={0} >
           <Center h="100%">
-            <Image src='/beerclicker.svg' alt='May the Beer be with you' w="14vh" />
+          <Image src={beerImage} alt='May the Beer be with you' w="16vh" />
           </Center>
         </Button>
-        {isRoundOnTheHouseVisible && (
-          <Button onClick={handleRoundOnTheHouseClick} fontFamily="American Typewriter" fontSize="lg" border="none" bgColor="green.400" color="white" mt={6} width="100%" borderRadius={0} >
-          Round on the house
-        </Button>
-        )}
       </Box>
+      {isRoundOnTheHouseVisible && (
+        <Button onClick={handleRoundOnTheHouseClick} fontFamily="American Typewriter" fontSize="lg" border="none" bgColor="green.400" color="white" mt={6} width="32vh" borderRadius={0} position="absolute" left={`${Math.floor(Math.random() * 70) + 15}%`} top={`${Math.floor(Math.random() * 70) + 15}%`}>
+          Round on the house
+        </Button>        
+      )}
       <Text fontFamily="American Typewriter" fontSize="xl" mt={6}>
         Beers drank: {beerCount}
       </Text>
-      <Button
-        onClick={handleResetClick}
-        backgroundColor='transparent'
-        _hover={{ backgroundColor: "transparent" }}
-        _active={{ backgroundColor: "red.600" }}
-        _focus={{ outline: "none" }}
-        border="none"
-        m={6}
-        p={0}
-      >
-        Reset
-      </Button>
       <Text fontFamily="American Typewriter" fontSize="xl" mt={6}>
         High Score: {highScore}
       </Text>
+      <Button onClick={handleResetClick} backgroundColor='transparent' _hover={{ backgroundColor: "transparent" }} _active={{ backgroundColor: "red.600" }} _focus={{ outline: "none" }} border="none" m={6} p={0} >
+        Reset
+      </Button>
     </Flex>
   );
 }
